b7417fcc89fca4c2333e58a362cfda7aa0441d1f
Juan Cuevas <jac.cuevas77@gmail.com>
2023-03-10 21:45:03 -0700

    Finished with messaging capabilities

--- message.js
+++ message.js
@@ -1,13 +1,10 @@
-class MessageList {
-    message;
-
-    constructor() {
-        this.messageList = [];
-        if (localStorage.getItem('messagesList') === null){
-            localStorage.setItem('messagesList', JSON.stringify(this.messageList));
-        }
+class Messages {
+    constructor(){
+        loadMessages();
     }
-    sendMessage(){
+}
+
+function sendMessage(){
         const messageInput = document.querySelector('#messageBox').value;
         const username = localStorage.getItem('userName');
         const dObj = new Date();
@@ -19,12 +16,42 @@ class MessageList {
         const messageDate = dObj.toLocaleDateString("en-US", options);
 
         const messageObj = {message: messageInput, date: messageDate, username: username};
-        let oldMsgList = JSON.parse(localStorage.getItem("messageList"));
+        let oldMsgList = JSON.parse(localStorage.getItem("messagesList"));
         oldMsgList.push(messageObj);
-        localStorage.setItem('messageList', JSON.stringify(oldMsgList));
+        localStorage.setItem('messagesList', JSON.stringify(oldMsgList));
+    }
 
-        let test = JSON.parse(localStorage.getItem("messageList"));
+function loadMessages() {
+    if (localStorage.getItem('messagesList') === null){
+        localStorage.setItem('messagesList', JSON.stringify([]));
+    }
+
+    const messagesData = JSON.parse(localStorage.getItem("messagesList"));
+    
+    const tableEl = document.querySelector("#messageTable");
+
+    if (messagesData.length > 0){
+        for (let i = messagesData.length - 1; i >= 0; i--) {
+            const senderEl = document.createElement('td');
+            const dateEl = document.createElement('td');
+            const messageEl = document.createElement('td');
+
+            const username = messagesData[i].username;
+            const date = messagesData[i].date;
+            const message = messagesData[i].message;
+
+            senderEl.textContent = username;
+            dateEl.textContent = date;
+            messageEl.textContent = message;
+
+            const rowEl = document.createElement('tr');
+                rowEl.appendChild(senderEl);
+                rowEl.appendChild(dateEl);
+                rowEl.appendChild(messageEl);
+
+            tableEl.appendChild(rowEl);
+        }
     }
 }
 
-const messages = new MessageList();
+const messages = new Messages();
