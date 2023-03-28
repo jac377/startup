78f959df40df466da6afff6e84585437cfce8795
Juan Cuevas <jac.cuevas77@gmail.com>
2023-03-27 23:56:58 -0600

    Added websocket to message tab

--- public/message.js
+++ public/message.js
@@ -1,57 +1,48 @@
-class Messages {
-    constructor(){
-        loadMessages();
-    }
+const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+socket.onopen = (event) => {
+    addMsg('system', 'Chat', 'Connected. Ready to start chatting');
+};
+
+socket.onmessage = async (event) => {
+    const text = await event.data.text();
+    const chat = JSON.parse(text);
+    addMsg('friend', chat.name, chat.msg, chat.date);
+};
+
+socket.onclose = (event) => {
+    addMsg('system', 'Chat', 'disconnected');
+    document.querySelector('#msgBtn').disabled = true;
+};
+
+function addMsg (cls, from, msg, date = '') {
+    const chatText = document.querySelector('#chat-text');
+    chatText.innerHTML = `<div><span class="${cls}">${date} - ${from}</span>: ${msg}</div>` + chatText.innerHTML;
 }
 
 function sendMessage(){
-        const messageInput = document.querySelector('#messageBox').value;
-        const username = localStorage.getItem('userName');
-        const dObj = new Date();
-        let options = {
-            year: "numeric",
-            month: "short",
-            day: "numeric" 
-        };
-        const messageDate = dObj.toLocaleDateString("en-US", options);
-
-        const messageObj = {message: messageInput, date: messageDate, username: username};
-        let oldMsgList = JSON.parse(localStorage.getItem("messagesList"));
-        oldMsgList.push(messageObj);
-        localStorage.setItem('messagesList', JSON.stringify(oldMsgList));
-    }
-
-function loadMessages() {
-    if (localStorage.getItem('messagesList') === null){
-        localStorage.setItem('messagesList', JSON.stringify([]));
-    }
-
-    const messagesData = JSON.parse(localStorage.getItem("messagesList"));
-    
-    const tableEl = document.querySelector("#messageTable");
-
-    if (messagesData.length > 0){
-        for (let i = messagesData.length - 1; i >= 0; i--) {
-            const senderEl = document.createElement('td');
-            const dateEl = document.createElement('td');
-            const messageEl = document.createElement('td');
-
-            const username = messagesData[i].username;
-            const date = messagesData[i].date;
-            const message = messagesData[i].message;
-
-            senderEl.textContent = username;
-            dateEl.textContent = date;
-            messageEl.textContent = message;
-
-            const rowEl = document.createElement('tr');
-                rowEl.appendChild(senderEl);
-                rowEl.appendChild(dateEl);
-                rowEl.appendChild(messageEl);
-
-            tableEl.appendChild(rowEl);
-        }
+    const msgEl = document.querySelector('#messageBox');
+    const msg = msgEl.value;
+    const username = localStorage.getItem('userName');
+    const dObj = new Date();
+    let options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric" 
+    };
+    const date = dObj.toLocaleDateString("en-US", options);
+    if (!!msg) {
+        addMsg('Me', 'Me', msg, date);
+        socket.send(`{"name":"${username}", "msg":"${msg}"}, "date":"${date}"`);
+        msgEl.value = '';
     }
 }
 
-const messages = new Messages();
+const input = document.querySelector('#messageBox');
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        sendMessage();
+        msgEl.value = '';
+    }
+});
